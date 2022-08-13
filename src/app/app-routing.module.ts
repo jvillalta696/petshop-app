6bae5132a414f510aa8502d5cd4e523041f550d7
jvillalta696 <jvillalta696@gmail.com>
2022-08-12 19:35:34 -0600

    Create DashboardView and fixed app-routing-model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Views/dashboard/dashboard.component';
 import { LoginComponent } from './Views/login/login.component';
 import { MainComponent } from './Views/main/main.component';
 import { PetAdopterComponent } from './Views/pet-adopter/pet-adopter.component';
@@ -8,25 +9,30 @@ import { SignInComponent } from './Views/sign-in/sign-in.component';
 import { UserprofileComponent } from './Views/userprofile/userprofile.component';
 
 const routes: Routes = [
-  { path: "", 
-  component: MainComponent 
-  },
   {
     path:"login",
     component: LoginComponent
   },
+
   {
-    path:"petadopter",
-    component:PetAdopterComponent
-  },
-  {
-    path:"petshop",
-    component:PetshopComponent
-  },
-  {
-    path:"userprofile",
-    component:UserprofileComponent
-  },
+    path:"",
+    component:DashboardComponent,
+    children:[
+      {
+        path:"petadopter",
+        component:PetAdopterComponent
+      },
+      {
+        path:"petshop",
+        component:PetshopComponent
+      },
+      {
+        path:"userprofile",
+        component:UserprofileComponent
+      },
+    ]
+  },  
+ 
   {
     path:"signin",
     component:SignInComponent    
