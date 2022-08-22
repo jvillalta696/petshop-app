325f2e55b92f902e1d60e0a4886eb44a3df0dd69
Bryan Bustillo <bryanbusti@gmail.com>
2022-08-21 18:06:32 -0600

    Bryan/Registrar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
     projectId: 'pet-shop-project-67f49',
     appId: '1:941790664899:web:44998bb16c91d8c7842a76',
     storageBucket: 'pet-shop-project-67f49.appspot.com',
+    locationId: 'us-central',
     apiKey: 'AIzaSyALANMUZ9LLIrPLzGA-m3UjjlZEiLGOmEs',
     authDomain: 'pet-shop-project-67f49.firebaseapp.com',
     messagingSenderId: '941790664899',
