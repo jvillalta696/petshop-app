9396e22aa242f3d82c67cd6f8e69ebbf63fb1441
jvillalta696 <jvillalta696@gmail.com>
2022-08-22 23:06:15 -0600

    Modify navigation to Sigup

--- src/app/Views/login/login.component.ts
+++ src/app/Views/login/login.component.ts
@@ -2,55 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserAuth } from 'src/app/Interfaces/user-auth';
-import {UserAuthService} from '../../Services/user-auth.service'
+import { UserAuthService } from '../../Services/user-auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-
 export class LoginComponent implements OnInit {
-  loginForm:FormGroup;
-  user:UserAuth;
-  
-  constructor(
-    private userAuth:UserAuthService,
-    private router:Router) { 
+  loginForm: FormGroup;
+  user: UserAuth;
+
+  constructor(private userAuth: UserAuthService, private router: Router) {
     this.loginForm = new FormGroup({
-      email:new FormControl(''),
-      psw: new FormControl('')      
+      email: new FormControl(''),
+      psw: new FormControl(''),
     });
-    this.user = {email:'',psw:''}
-  }
-  
-  ngOnInit(): void {
+    this.user = { email: '', psw: '' };
   }
 
-  signin(){
+  ngOnInit(): void {}
+
+  signin() {
     this.user = this.loginForm.value;
-    this.userAuth.signin(this.user)
-    .then(res=>{
-      console.log(res);      
-      alert('Login Successfully'),
-      this.router.navigate([''])
-    })
-    .catch(err=>{
-      console.log(err);     
-    })    
+    this.userAuth
+      .signin(this.user)
+      .then((res) => {
+        console.log(res);
+        alert('Login Successfully'), this.router.navigate(['']);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  signup(){
-    this.user = this.loginForm.value;
-    this.userAuth.signup(this.user)
-    .then(res=>{
-      console.log(res)
-      alert('SignUp Successfully'),
-      this.router.navigate([''])
-      
-    })
-    .catch(err=>{
-      console.log(err);     
-    })
+  signup() {
+    this.router.navigate(['signin']);
   }
 }
